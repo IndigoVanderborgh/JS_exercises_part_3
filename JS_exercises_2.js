1c1463fc116519a0ac51559fa74cca62d97256d9
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-22 23:50:28 -0700

    Create JS_exercises_2.js

---
+++ JS_exercises_2.js
@@ -0,0 +1,13 @@
+// How to check truth or false? we use boolean:
+
+// let trueOfFalse = Boolean("hello")
+
+// console.log(trueOfFalse)
+
+//  try some challenges:
+console.log(  Boolean("")   ) //
+console.log(  Boolean("0")  ) //
+console.log(  Boolean(100)  ) //
+console.log(  Boolean(null) ) //
+console.log(  Boolean([0])  ) //
+console.log(  Boolean(-0)   ) //
