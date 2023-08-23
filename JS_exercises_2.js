978deb8925e6858e6a91e02e955d08cba6e4168a
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-22 23:55:10 -0700

    Update JS_exercises_2.js

--- JS_exercises_2.js
+++ JS_exercises_2.js
@@ -5,9 +5,17 @@
 // console.log(trueOfFalse)
 
 //  try some challenges:
-console.log(  Boolean("")   ) //
-console.log(  Boolean("0")  ) //
-console.log(  Boolean(100)  ) //
-console.log(  Boolean(null) ) //
-console.log(  Boolean([0])  ) //
-console.log(  Boolean(-0)   ) //
+console.log(  Boolean("")   ) // false
+console.log(  Boolean("0")  ) // true
+console.log(  Boolean(100)  ) // true
+console.log(  Boolean(null) ) // false
+console.log(  Boolean([0])  ) // true the array takes precedence
+console.log(  Boolean(-0)   ) // false, negatives and positives are unaffected in truth/false
+
+// all we need to know is the 6 falsy statements in JS:
+// false
+// 0
+// ""
+// null
+// undefined
+// NaN
