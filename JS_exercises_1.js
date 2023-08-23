e0efc77ab149e8e3d1c64a78821f04db93a935d4
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-22 23:01:27 -0700

    Update JS_exercises_1.js

--- JS_exercises_1.js
+++ JS_exercises_1.js
@@ -2,11 +2,21 @@
 
 const credits = 0 // initial credits
 
-if (credits > 0) {
+// if (credits > 0) {
+//     console.log("Let's play 🎰") // allowed to play credits higher than 0
+// } else {
+//     console.log("Sorry, you have no credits 😭") // no credit = no play
+// }
+
+// truthy
+// falsy
+
+// if we replaced the credits to string "yolo" it still runs through.
+if (true) {
     console.log("Let's play 🎰") // allowed to play credits higher than 0
 } else {
     console.log("Sorry, you have no credits 😭") // no credit = no play
 }
 
-// truthy
-// falsy
+// all we need to know is the falsy statements:
+// 
