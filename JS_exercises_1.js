c997c99899b8961ae21a96fa8011a9b82cb77a15
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-22 22:56:14 -0700

    Update JS_exercises_1.js

--- JS_exercises_1.js
+++ JS_exercises_1.js
@@ -1,7 +1,9 @@
-const credits = 0
+// we're going to learn about truthy and falsey values, using a slot machine example
+
+const credits = 0 // initial credits
 
 if (credits > 0) {
-    console.log("Let's play 🎰")
+    console.log("Let's play 🎰") // 
 } else {
-    console.log("Sorry, you have no credits 😭")
+    console.log("Sorry, you have no credits 😭") // 
 }
