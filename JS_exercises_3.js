63b584b92b187762467fe1b1a78884a284fbb623
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-24 18:10:34 -0700

    Update JS_exercises_3.js

--- JS_exercises_3.js
+++ JS_exercises_3.js
@@ -11,5 +11,32 @@
 
 // greetUser()
 
-// we'll enter in name in the function greetUser() parameter on line 8
+// we'll enter in name in the function greetUser() parameter on line 8:
 
+// const welcomeEl = document.getElementById("welcome-el")
+
+// function greetUser(name) {
+//     welcomeEl.textContent = "Welcome back, Per Harald Borgen 👋"    
+// }
+
+// greetUser()
+
+// we are now expected to use name in the function. see line 29
+
+// // const welcomeEl = document.getElementById("welcome-el")
+
+// function greetUser() {
+//     welcomeEl.textContent = "Welcome back, " + name + " 👋"    
+// }
+
+// greetUser()
+
+// it will return erros, because we need to pass it on in the greetUser() as well.
+
+// // const welcomeEl = document.getElementById("welcome-el")
+
+// function greetUser() {
+//     welcomeEl.textContent = "Welcome back, " + name + " 👋"    
+// }
+
+// greetUser()
